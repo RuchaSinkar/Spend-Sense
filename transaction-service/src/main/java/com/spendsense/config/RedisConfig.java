6691687b28b11641540051b733cd92009c2323d0
RuchaSinkar <ruchasinkar15@gmail.com>
2026-07-01 17:27:04 +0530

    updated redis config

--- transaction-service/src/main/java/com/spendsense/config/RedisConfig.java
+++ transaction-service/src/main/java/com/spendsense/config/RedisConfig.java
@@ -1,16 +1,27 @@
 package com.spendsense.config;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Configuration
 public class RedisConfig {
+
+    @Value("${spring.data.redis.url:NOT_SET}")
+    private String redisUrl;
 
     @Bean
     public StringRedisTemplate stringRedisTemplate(
             RedisConnectionFactory factory) {
         return new StringRedisTemplate(factory);
+    }
+    @PostConstruct
+    public void logRedisUrl() {
+        log.info("🔍 Redis URL configured as: {}", redisUrl);
     }
 }
